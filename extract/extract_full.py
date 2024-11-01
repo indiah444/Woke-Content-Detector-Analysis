61874a0b749f31fb09a82b978938527523627aef
India <india.howell123@gmail.com>
2024-11-01 11:36:24 +0000

    Function to download video game sales Kaggle dataset.

--- extract/extract_full.py
+++ extract/extract_full.py
@@ -1,10 +1,13 @@
 """A file to extract the entire Woke Content Detector list and other datasets to CSVs."""
 
+import os
 from os import environ as ENV
+from os import rename
 import gspread
 import pandas as pd
 from dotenv import load_dotenv
 from oauth2client.service_account import ServiceAccountCredentials
+import kagglehub
 
 
 def download_wcd_google_sheet(sheet_url, csv_file_path):
@@ -28,10 +31,19 @@ def download_wcd_google_sheet(sheet_url, csv_file_path):
     print(f"CSV file downloaded successfully and saved to {csv_file_path}")
 
 
-def download_vg_sales_kaggle():
+def download_vg_sales_kaggle(dataset_name: str, download_path: str):
     """Download data from a Kaggle dataset and save it as a CSV."""
 
-    pass
+    load_dotenv()
+
+    dataset_folder_path = kagglehub.dataset_download(
+        dataset_name, force_download=True)
+
+    downloaded_file_path = os.path.join(
+        dataset_folder_path, os.listdir(dataset_folder_path)[0])
+
+    rename(downloaded_file_path, download_path)
+    print(f"Dataset downloaded and saved to {download_path}")
 
 
 if __name__ == "__main__":
@@ -39,5 +51,9 @@ if __name__ == "__main__":
     load_dotenv()
 
     google_sheet = "https://docs.google.com/spreadsheets/d/1AVTZPJij5PQmlWAkYdDahBrxDiwqWMGsWEcEnpdKTa4/edit?gid=0"
-    csv_filepath = "woke_content_detector_full.csv"
-    download_wcd_google_sheet(google_sheet, csv_filepath)
+    wcd_csv_filepath = "woke_content_detector_full.csv"
+    download_wcd_google_sheet(google_sheet, wcd_csv_filepath)
+
+    vg_dataset_name = "gregorut/videogamesales"
+    vg_csv_filepath = "videogame_sales.csv"
+    download_vg_sales_kaggle(vg_dataset_name, vg_csv_filepath)
