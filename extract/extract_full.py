705d4766edf7df89372ee0c1db6eba88e2eb6249
India <india.howell123@gmail.com>
2024-11-01 12:04:04 +0000

    Adding error handling and logging.

--- extract/extract_full.py
+++ extract/extract_full.py
@@ -3,13 +3,19 @@
 import os
 from os import environ as ENV
 from os import rename
+import logging
 
 import gspread
+from gspread.exceptions import SpreadsheetNotFound
 import pandas as pd
 from dotenv import load_dotenv
 from oauth2client.service_account import ServiceAccountCredentials
 import kagglehub
 
+from logging_config import logger_setup
+
+
+LOGGER = logging.getLogger(__name__)
 
 WCD_GOOGLE_SHEET = "https://docs.google.com/spreadsheets/d/1AVTZPJij5PQmlWAkYdDahBrxDiwqWMGsWEcEnpdKTa4/edit?gid=0"
 WCD_CSV_FILEPATH = "woke_content_detector_full.csv"
@@ -21,40 +27,65 @@ VG_CSV_FILEPATH = "videogame_sales.csv"
 def download_wcd_google_sheet(sheet_url, csv_file_path):
     """Download the Woke Content Detector data from a Google Sheet and save it as CSV."""
 
-    scope = ["https://spreadsheets.google.com/feeds",
-             "https://www.googleapis.com/auth/drive"]
+    try:
+        scope = ["https://spreadsheets.google.com/feeds",
+                 "https://www.googleapis.com/auth/drive"]
 
-    json_key_path = ENV["GOOGLE_SHEET_PATH"]
+        json_key_path = ENV["GOOGLE_SHEET_PATH"]
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        json_key_path, scope)
-    client = gspread.authorize(creds)
+        creds = ServiceAccountCredentials.from_json_keyfile_name(
+            json_key_path, scope)
+        client = gspread.authorize(creds)
 
-    sheet = client.open_by_url(sheet_url).sheet1
+        sheet = client.open_by_url(sheet_url).sheet1
 
-    data = sheet.get_all_values()
-    df = pd.DataFrame(data[1:], columns=data[0])
+        data = sheet.get_all_values()
+        if not data:
+            LOGGER.warning("No data retrieved from Google Sheet.")
+            return
 
-    df.to_csv(csv_file_path, index=False)
-    print(f"CSV file downloaded successfully and saved to {csv_file_path}")
+        df = pd.DataFrame(data[1:], columns=data[0])
+        df.to_csv(csv_file_path, index=False)
+        LOGGER.info(
+            "CSV file downloaded successfully and saved to %s", csv_file_path)
+
+    except SpreadsheetNotFound:
+        LOGGER.error(
+            "Google Sheet not found. Please check the URL and try again.")
+    except KeyError:
+        LOGGER.error("Environment variable 'GOOGLE_SHEET_PATH' not found.")
+    except Exception as e:
+        LOGGER.exception(
+            "An error occurred while downloading Google Sheet: %s", e)
 
 
 def download_vg_sales_kaggle(dataset_name: str, download_path: str):
     """Download data from a Kaggle dataset and save it as a CSV."""
 
-    dataset_folder_path = kagglehub.dataset_download(
-        dataset_name, force_download=True)
+    try:
+        dataset_folder_path = kagglehub.dataset_download(
+            dataset_name, force_download=True)
+        downloaded_file_path = os.path.join(
+            dataset_folder_path, os.listdir(dataset_folder_path)[0])
 
-    downloaded_file_path = os.path.join(
-        dataset_folder_path, os.listdir(dataset_folder_path)[0])
+        rename(downloaded_file_path, download_path)
+        LOGGER.info("Dataset downloaded and saved to %s", download_path)
 
-    rename(downloaded_file_path, download_path)
-    print(f"Dataset downloaded and saved to {download_path}")
+    except FileNotFoundError:
+        LOGGER.error(
+            "The specified Kaggle dataset could not be found: %s", dataset_name)
+    except Exception as e:
+        LOGGER.exception(
+            "An error occured while downloading Kaggle dataset: %s", e)
 
 
 if __name__ == "__main__":
 
+    logger_setup("extract_full_log.log", "logs")
     load_dotenv()
+    LOGGER.info("Loading environment variables from .env file.")
 
+    LOGGER.info("Starting data extraction process.")
     download_wcd_google_sheet(WCD_GOOGLE_SHEET, WCD_CSV_FILEPATH)
     download_vg_sales_kaggle(VG_DATASET_NAME, VG_CSV_FILEPATH)
+    LOGGER.info("Data extraction process completed.")
